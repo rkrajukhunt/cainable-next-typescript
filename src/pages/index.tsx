18a4eeea98b577c865d53c800e24bc068ab14f26
ebmehulmalaviya <105291127+ebmehulmalaviya@users.noreply.github.com>
2022-09-02 20:48:50 +0530

    ok

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import Card   from "../components/card";
+import Card2   from "../components/care2";
 export default function Home() {
   return (
     <div className="container flex items-center p-4 mx-auto min-h-screen justify-center">
       <main>
-        
+        <div>
         <Card></Card>
+        </div>
+        <div>
+        <Card2></Card2>
+        </div>
         
       </main>
     </div>
