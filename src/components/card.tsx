1584fb16769f29c89d41549774175a9e02b0b0aa
Raju Khunt <rajukhunt15@gmail.com>
2022-09-02 19:05:21 +0530

    [ADD] componets

---
+++ src/components/card.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+    return (
+      <div className="container flex items-center p-4 mx-auto min-h-screen justify-center">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+  <img className="w-full" src="assets/main.png" alt="Kyoukai no Kanata"></img>
+  
+  <div className="px-6 pt-4 pb-2">
+    <p>Kyoukai no Kanata</p>
+  </div>
+</div>
+      </div>
+    )
+  }
